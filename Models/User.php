96b44154444e0db9604e946d6e233dbd2138804a
RAYID DJERI <58982423+dR-Corp@users.noreply.github.com>
2024-02-20 19:01:12 +0100

    Creation du model user

---
+++ Models/User.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 
+ * class User
+ */
+class User extends Model {
+
+    // const ROLE_USER = "USER";
+    // const ROLE_SUPER = "GOKU";
+
+    protected $fillable = ['username', 'password', 'structure_ID', 'role'];
+
+
+    public function structure() {
+        return Structure::find($this->getId());
+    }
+
+
+    // the password must be hash
+    public static function create(array $attributes = [])
+    {
+        
+        if(isset($attributes['password'])) 
+            $attributes['password'] = sha1($attributes['password']);
+        
+        return parent::create($attributes);
+
+    }
+
+    public function update(array $attributes = [])
+    {
+
+        if(isset($attributes['password'])) 
+            $attributes['password'] = sha1($attributes['password']);
+        else {
+            unset($attributes['password']);
+        }
+
+        return parent::update($attributes);
+        
+    }
+    
+}
